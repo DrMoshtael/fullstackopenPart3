7f35eb1aef330e2951a8a3ee9c7d72f62a74081f
Howard <howard@brightpage.tech>
2024-01-01 21:21:30 +0000

    Exercise 3.18

--- index.js
+++ index.js
@@ -56,15 +56,26 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people.
-    <br/><br/> ${Date()}`)
+    Person.countDocuments({}).then(result => {
+        response.send(`Phonebook has info for ${result} people.
+        <br/><br/> ${Date()}`)
+    })
+    // console.log("count",count,typeof(count),"counted")
+    // response.send(`Phonebook has info for ${count} people.
+    // <br/><br/> ${Date()}`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findByID(request.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
         .then(person => {
-            response.json(person)
+            if (person) {
+                response.json(person)
+            }
+            else {
+                response.status(404).end()
+            }
         })
+        .catch(error => next(error))
 
     // const id=Number(request.params.id)
     // const person=persons.find(person=>person.id===id)
@@ -128,6 +139,28 @@ app.post('/api/persons', (request, response) => {
 
     // response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandling = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response
+            .status(400)
+            .send({
+                error: 'malformatted id'
+            })
+    }
+
+    next(error)
+}
+
+app.use(errorHandling)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
